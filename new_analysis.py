b4ac282f90943f83533380870ca6209a337eb38e
Syed Mahammad Saleem <syedmahammadsaleem@gmail.com>
2026-03-27 19:21:56 +0530

    Add new analysis script on feature branch

--- new_analysis.py
+++ new_analysis.py
@@ -4,10 +4,10 @@
 import pandas as pd
 import numpy as np
 
-# simple Data Science analysis
-data=[10,20,30,40,50]
+# Sample Data Science analysis
+data = [10, 20, 30, 40, 50]
 
 print("New Analysis Results:")
 print(f"Mean: {np.mean(data)}")
-print(f"Max:{np.max(data)}")
-print(f"Min:{np.min(data)}")
+print(f"Max: {np.max(data)}")
+print(f"Min: {np.min(data)}")
